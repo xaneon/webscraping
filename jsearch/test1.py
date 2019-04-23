d40f53a4b7e53a497674028c3e3301ee8846da56
Bonne <bonne.habekost@gmail.com>
2019-04-23 12:21:12 +0200

    first version with selenium and bs4

--- jsearch/test1.py
+++ jsearch/test1.py
@@ -2,6 +2,8 @@ import requests
 import getpass
 from myinfo import *
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
 
 loginurl = f"{indeed_baseurl}/account/login"
 
@@ -14,20 +16,25 @@ url = (f"{indeed_baseurl}/Jobs?as_and={with_all_words}"+
        f"&limit={results_per_page}&sort={sort_by}&psf=advsrch")
 
 headers = {'User-Agent': 'Mozilla/5.0',  'Content-type': 'application/html'}
-payload = {'username': username, 'pass': getpass.getpass()}
+# payload = {'username': username, 'pass': getpass.getpass()}
+password = getpass.getpass(prompt="Indeed Password: ")
 
-driver = webdriver.Firefox()
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 
 driver.get(loginurl)
-a = driver.find_element_by_id("login-email-input")
-print(type(a))
+driver.find_element_by_id("login-email-input").send_keys(username)
+driver.find_element_by_id("login-password-input").send_keys(password)
+driver.find_element_by_id("login-submit-button").click()
 
-# session = requests.session()
-# session.post(loginurl, headers=headers, data=payload)
-# r = session.get(url)
-# r = requests.get(url, headers=headers)
-# print(r.text)
+driver.get(url)
+source = driver.page_source
+driver.close()
 
-# with open("response.html", "w") as fid:
-#     fid.write(r.text)
+soup = BeautifulSoup(source, "html.parser")
+print(soup)
+
+with open("response.html", "w") as fid:
+     fid.write(source)
 
