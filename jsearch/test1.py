0d014479840ee0c609fbc00d5f5ddd596c9ec848
Bonne <bonne.habekost@gmail.com>
2019-04-23 15:47:41 +0200

    first version of extraction working

--- jsearch/test1.py
+++ jsearch/test1.py
@@ -4,6 +4,9 @@ from myinfo import *
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+import re
+import json
+from collections import defaultdict
 
 loginurl = f"{indeed_baseurl}/account/login"
 
@@ -15,8 +18,6 @@ url = (f"{indeed_baseurl}/Jobs?as_and={with_all_words}"+
        f"&radius={radius}&l={location}&fromage={fromage}"+
        f"&limit={results_per_page}&sort={sort_by}&psf=advsrch")
 
-headers = {'User-Agent': 'Mozilla/5.0',  'Content-type': 'application/html'}
-# payload = {'username': username, 'pass': getpass.getpass()}
 password = getpass.getpass(prompt="Indeed Password: ")
 
 options = Options()
@@ -33,7 +34,30 @@ source = driver.page_source
 driver.close()
 
 soup = BeautifulSoup(source, "html.parser")
-print(soup)
+l = re.findall("(jobmap.*)= ({.+})", source) # get joblist with ids
+d = defaultdict(dict)
+for idx, (mapname, s) in enumerate(l):
+    # curr = json.loads(re.sub(r'([a-zA-Z]+):', r' "\1": ', s.replace("\'", "\"")))
+    itemlist = re.sub(r"[{}]", r"", s.replace("'", "\"")).split(",")
+    for elem in itemlist:
+        tmp = elem.split(":")
+        key, value = tmp[:2]
+        d[idx][key] = value[1:-1] # remove " at the beg and end
+
+for entry in d:
+    d[entry]["res"] = soup.find("div", attrs={'id':"p_" + d[0]["jk"]})
+    ts = d[entry]["res"].find("div", attrs={"class": "title"}).text.strip()
+    company = d[entry]["res"].find("span", attrs={"class": "company"}).text.strip()
+    d[entry]["title" + "_scraped"] = ts
+    d[entry]["company" + "_scraped"] = company
+    # TODO: summary, a href and date
+
+
+print(d)
+
+# TODO: get joblist, and afertwards find contents by ID
+# TODO: write function for indeed and then monster etc
+# Write Jobs to Excel table with pandas
 
 with open("response.html", "w") as fid:
      fid.write(source)
